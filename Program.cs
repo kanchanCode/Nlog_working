4de959fc4fe0e28ac8deb37dbb8156ac6c8bb2fe
Kanchan Jaiswal <91010576+kanchanCode@users.noreply.github.com>
2021-10-27 12:56:45 +0530

    added code and files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace classwork_Nlog
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            nlog nlogObject = new nlog();
+            nlogObject.LogDebug("Debugging the code");
+
+            addNum obj2 = new addNum();
+            obj2.sum(0, 0);
+        }
+    }
+}
---
+++ addNum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classwork_Nlog
+{
+   public class addNum
+    {
+        nlog log = new nlog();
+
+        public int sum(int a,int b)
+        {
+            log.LogInfo("a : " + a + " " + "b :" + b);
+            if (a == 0 || b == 0)
+            {
+                log.LogDebug("debug is successful");
+                log.LogError("Error in a or b's value ");
+                return 0;
+            }
+            int c = a + b;
+            log.LogDebug("debug done ");
+            log.LogDebug("sum is "+c);
+            return c;
+        }
+    }
+}
